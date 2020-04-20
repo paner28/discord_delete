f1219305386e2b185e08e244a81fb49ac4cb8322
paner28 <61655946+paner28@users.noreply.github.com>
2020-04-20 22:10:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,21 @@
 from discord.ext import commands
 import discord
 import os
-import sympy
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
-# インストールした discord.py を読み込む
-import discord
-
-# 起動時に動作する処理
 @client.event
 async def on_ready():
-  channel = client.get_channel(const.channel_id['bot_control'])
-  # 起動したらターミナルにログイン通知が表示される
-  print('投稿削除サーバー')
+  channel = client.get_channel(701731353783304225)
   await channel.send('投稿削除サーバー起動')
   return
 
-# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-  if message.name == "bot_control":
+  if message.channel.name == "bot_control":
+    channel = client.get_channel(701731353783304225)
+    await channel.send("削除を開始します")
     if message.content == "delete":
       if message.author.bot:
         return
@@ -36,7 +30,7 @@ async def on_message(message):
       if message.channel.name == '対戦b実況':
         return
       await message.channel.purge()
-      print('投稿削除しました。')
+      await channel.send("完了しました")
+      return
 
-# Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
